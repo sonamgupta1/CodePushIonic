ed79763c6b580197e3b43fe3126ed7cb097b0965
Sonam <Sonam>
2015-11-21 18:20:20 +0530

    cordova js added

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var CodePushIonic = angular.module('starter', ['ionic'])
+var CodePushIonic = angular.module('starter', ['ionic','ngCordova'])
 
 CodePushIonic.run(function ($ionicPlatform, $ionicLoading, $rootScope) {
     $ionicPlatform.ready(function () {

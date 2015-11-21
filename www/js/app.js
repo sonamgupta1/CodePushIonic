2d6a7c9d0ca7a813673c8a22cffb57a51c12602b
Sonam <Sonam>
2015-11-21 17:26:43 +0530

    Blank app with plugin

---
+++ www/js/app.js
@@ -0,0 +1,62 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var CodePushIonic = angular.module('starter', ['ionic'])
+
+CodePushIonic.run(function ($ionicPlatform, $ionicLoading, $rootScope) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+        var onError = function (error) {
+            $rootScope.$broadcast('loading:hide')
+
+            console.log("An error occurred. " + error);
+        };
+
+        var onApplySuccess = function () {
+            $rootScope.$broadcast('loading:hide')
+            console.log("Apply succeeded. Reloading the application...");
+        };
+
+        var onPackageDownloaded = function (localPackage) {
+            $rootScope.$broadcast('loading:show')
+            localPackage.apply(onApplySuccess, onError);
+        };
+
+        var onUpdateCheck = function (remotePackage) {
+            $rootScope.$broadcast('loading:show')
+            if (!remotePackage) {
+                $rootScope.$broadcast('loading:hide')
+
+                console.log("The application is up to date.");
+            } else {
+                $rootScope.$broadcast('loading:hide')
+
+                console.log("A CodePush update is available. Package hash: " + remotePackage.packageHash);
+                remotePackage.download(onPackageDownloaded, onError);
+            }
+        };
+
+        window.codePush.checkForUpdate(onUpdateCheck, onError);
+        $rootScope.$on('loading:show', function () {
+            $ionicLoading.show({
+                template: '<p class="center">' +
+                    '<ion-spinner icon="android"/>' +
+                    '<div>Loading...</div>' +
+                    '</p>'
+            })
+        });
+
+        $rootScope.$on('loading:hide', function () {
+            $ionicLoading.hide()
+        })
+    });
+})

62d21ab250cd03ac124e2314ba0200138ce6a2e3
Sonam <Sonam>
2015-11-26 15:26:27 +0530

    take a pic and show

--- www/js/controllers/CameraCtrl.js
+++ www/js/controllers/CameraCtrl.js
@@ -1,55 +1,31 @@
-CodePushIonic.controller("CameraCtrl", function ($scope, $cordovaCamera) {
+CodePushIonic.controller("CameraCtrl", function($scope, $cordovaCamera) {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////        To take a pic and show on the first page        /////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    $scope.takePhoto = function () {
+    $scope.takePicture = function() {
         var options = {
-            quality: 75,
-            destinationType: Camera.DestinationType.DATA_URL,
-            sourceType: Camera.PictureSourceType.CAMERA,
-            allowEdit: true,
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
             encodingType: Camera.EncodingType.JPEG,
             targetWidth: 300,
             targetHeight: 300,
             popoverOptions: CameraPopoverOptions,
             saveToPhotoAlbum: false
         };
+        $scope.images = [];
 
-        $cordovaCamera.getPicture(options).then(function (imageData) {
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+
             $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function (err) {
-            if (appConstants.debug) {
-                alert('Failed because: ' + message);
-            }
+
+            $scope.images.push($scope.imgURI);
+
+            console.log("length",$scope.images.length);
+
+
+        }, function(err) {
+            // An error occured. Show a message to the user
         });
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////        To choose a pic from gallery in and show on the first page        //////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    $scope.choosePhoto = function () {
-        var options = {
-            quality: 75,
-            destinationType: Camera.DestinationType.DATA_URL,
-            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-            allowEdit: true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
-
-        $cordovaCamera.getPicture(options).then(function (imageData) {
-            $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function (err) {
-            if (appConstants.debug) {
-                alert('Failed because: ' + message);
-            }
-        });
-    }
-
 
 });
